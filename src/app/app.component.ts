a3459b9da63fda82372cff4bb798dd933812f438
phanphuc2210 <phucpth2001@gmail.com>
2023-04-25 21:34:43 +0700

    update messenger and fix display error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component, OnInit} from '@angular/core';
-import { FacebookService, InitParams } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private facebookService: FacebookService) { }  
-
-  ngOnInit(): void {
-    this.initFacebookService();
-  }    
-  
-  private initFacebookService(): void {
-    const initParams: InitParams = { xfbml:true, version:'v16.0'};
-    this.facebookService.init(initParams);
-  }
+export class AppComponent {
 }
